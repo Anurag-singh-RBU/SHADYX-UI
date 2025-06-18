74b0b017e8e719c2f8f4c38979163ca689743040
Anurag-singh-RBU <anuragsingh260805@gmail.com>
2025-06-18 13:45:14 +0530

    added a new component

--- app/components/ui/Artifacts.tsx
+++ app/components/ui/Artifacts.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 
 const Artifacts = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 w-full px-4 sm:px-8">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 w-full px-4 sm:px-8 mono-text">
       {/* Card 1 */}
       <Link href="/parallaxcomponent" className="flex flex-col cursor-pointer">
         <div className="bg-black rounded-2xl overflow-hidden py-3">
           <div id="para" />
         </div>
-        <h1 className="text-center text-xl mt-6 mono-text font-bold">Parallax Effect</h1>
-        <h4 className="text-center font-GS text-sm mt-1 sm:font-medium">
+        <h1 className="text-center text-xl mt-6 font-bold">Parallax Effect</h1>
+        <h4 className="text-center text-sm mt-1 font-bold sm:font-medium">
           Move your cursor and watch it shift
         </h4>
       </Link>
@@ -20,8 +20,8 @@ const Artifacts = () => {
         <div className="bg-black rounded-2xl overflow-hidden py-3">
           <div id="paraTwo" />
         </div>
-        <h1 className="text-center text-xl mt-6 mono-text font-bold">Illumination Effect</h1>
-        <h4 className="text-center font-GS text-sm mt-1 sm:font-medium">
+        <h1 className="text-center text-xl mt-6 font-bold">Illumination Effect</h1>
+        <h4 className="text-center mono-text text-sm mt-1 font-bold sm:font-medium">
           Every glow and movement speaks your life
         </h4>
       </Link>
@@ -31,8 +31,8 @@ const Artifacts = () => {
         <div className="bg-black rounded-2xl overflow-hidden py-3">
           <div id="paraThree" />
         </div>
-        <h1 className="text-center text-xl mt-6 mono-text font-bold">Svg Masking</h1>
-        <h4 className="text-center font-GS text-sm mt-1 sm:font-medium">
+        <h1 className="text-center text-xl mt-6 font-bold">Svg Masking</h1>
+        <h4 className="text-center mono-text text-sm mt-1 font-bold sm:font-medium">
           Shape vision with beautiful SVG masks
         </h4>
       </Link>
@@ -42,8 +42,8 @@ const Artifacts = () => {
         <div className="bg-[rgb(82,82,82)] rounded-2xl overflow-hidden py-3">
           <div id="paraFour" />
         </div>
-        <h1 className="text-center text-xl mt-6 mono-text font-bold">Paint Board</h1>
-        <h4 className="text-center font-GS text-sm mt-1 sm:font-medium">
+        <h1 className="text-center text-xl mt-6 font-bold">Paint Board</h1>
+        <h4 className="text-center mono-text text-sm mt-1 font-bold sm:font-medium">
           Shape vision with beautiful SVG masks
         </h4>
       </Link>
