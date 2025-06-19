da4775a9085f0dea315086eb36c6d2132b4779a6
Anurag-singh-RBU <anuragsingh260805@gmail.com>
2025-06-19 23:38:18 +0530

    updated code block

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next"
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SquigglyUnderline } from "./SquigglyUnderline";
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         <SquigglyUnderline></SquigglyUnderline>
         {children}
+        <Analytics />
         <Footerdemo></Footerdemo>
       </body>
     </html>
