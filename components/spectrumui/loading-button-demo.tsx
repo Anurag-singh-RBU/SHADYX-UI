08b4a81302d3ca0bd89a7d78ac82e28ffcc258c1
Anurag-singh-RBU <anuragsingh260805@gmail.com>
2025-07-26 13:35:47 +0530

    CLI

--- components/spectrumui/loading-button-demo.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-// import { LoadingButton } from '@/components/ui/loading-button';
-import { LoadingButton } from "@/components/spectrumui/loading-button-dependencies";
-
-const LoadingButtonDemo = () => {
-  return <LoadingButton loading>loading button</LoadingButton>;
-};
-
-export default LoadingButtonDemo;
