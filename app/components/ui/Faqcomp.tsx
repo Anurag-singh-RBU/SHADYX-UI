92dfc54fc9646a4de99e0b8ec34c5b82aece5c33
Anurag-singh-RBU <anuragsingh260805@gmail.com>
2025-09-13 19:50:49 +0530

    added new component

--- app/components/ui/Faqcomp.tsx
+++ app/components/ui/Faqcomp.tsx
@@ -157,7 +157,7 @@ return (
           <div className="h-auto overflow-x-auto overflow-hidden">
             <CliOutput
               commands={{
-                npm: 'npx @anuragcodes/shadyx add https://shadyxui.in/r/not-found.json',
+                npm: 'npx @anuragcodes/shadyx add https://shadyxui.in/r/accordian.json',
                 pnpm: 'pnpm dlx @anuragcodes/shadyx add https://shadyxui.in/r/not-found.json',
                 yarn: 'yarn dlx @anuragcodes/shadyx add https://shadyxui.in/r/not-found.json',
                 bun: 'bunx @anuragcodes/shadyx add https://shadyxui.in/r/not-found.json',
